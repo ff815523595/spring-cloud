8cb95b57f06713b25a951b00d1aa954afac877d0
xk <815523595@qq.com>
2017-11-28 09:45:48 +0800

    cloud配置服务端与客户端

---
+++ service-config/src/main/java/com/application/ServiceConfigApplication.java
@@ -0,0 +1,20 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by a on 2017/11/27.
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ServiceConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceConfigApplication.class);
+    }
+
+}
--- service-feign/src/main/java/com/application/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/application/ServiceFeignApplication.java
@@ -3,18 +3,23 @@ package com.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by a on 2017/11/22.
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients("com.service.service0")
 @ComponentScan("com")
 @EnableHystrixDashboard
+@RefreshScope
+@RestController
 public class ServiceFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceFeignApplication.class);
