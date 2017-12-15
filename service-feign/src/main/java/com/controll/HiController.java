fc4422fe5e1735e2df736bc4efdd92674ac65c67
xk <815523595@qq.com>
2017-12-15 17:32:39 +0800

    打印注册服务信息

--- service-feign/src/main/java/com/controll/HiController.java
+++ service-feign/src/main/java/com/controll/HiController.java
@@ -1,11 +1,17 @@
 package com.controll;
 
 import com.service.service0.SchedualServiceHi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by a on 2017/11/22.
@@ -13,11 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HiController.class);
     @Autowired
     SchedualServiceHi schedualServiceHi;
+    @Autowired
+    DiscoveryClient client;
 
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     public String sayHi(@RequestParam String name){
+        List<String> services =  client.getServices();
+        for (String service : services) {
+            logger.info("注册的服务信息："+service.toString());
+        }
         return schedualServiceHi.sayHiFromClientOne(name);
     }
 }
